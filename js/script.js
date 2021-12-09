792109a5a3402d223223063ce7fdbb54e9fc27a1
Alessio2021 <alessiobut1911@gmail.com>
2021-12-09 15:33:26 +0100

    bonus

--- js/script.js
+++ js/script.js
@@ -53,3 +53,42 @@ for (let i = 0; i < team.length; i++){
 
   container.innerHTML += element;
 }
+
+const button = document.getElementById('addMemberButton');
+const idUser = document.getElementById('name');
+const roleUser = document.getElementById('role');
+const imageUser = document.getElementById('image');
+
+button.addEventListener('click', function(event){
+  event.preventDefault();
+
+  const nameUser = idUser.value;
+  const role = roleUser.value;
+  const image = imageUser.value;
+
+  const newCard = {
+    name: nameUser,
+    role: role,
+    image: image
+  };
+
+  team.push(newCard);
+  console.log(team);
+  const element =
+
+  `
+  <div class="team-card">
+  <div class="card-image">
+    <img
+      src="img/${newCard.image}"
+      alt="${newCard.name}"
+    />
+  </div>
+  <div class="card-text">
+    <h3>${newCard.name}</h3>
+    <p>${newCard.role}</p>
+  </div>
+  `
+
+  container.innerHTML += element;
+});
